474b0c2a43be0f097cee0560cf4c017919dd4328
osmanforhad <developer.osmanforhad@gmail.com>
2022-06-06 23:28:24 +0600

    dashboard template setup

--- resources/views/dashboards/admins/settings.blade.php
+++ resources/views/dashboards/admins/settings.blade.php
@@ -1,26 +1,6 @@
-@extends('layouts.app')
+@extends('dashboards.admins.layouts.admin-dashboard-layout')
+@section('title', 'Admin || Settings')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-                <h1>Hello : {{ Auth::user()->name }}</h1>
-                <hr>
-                Admin Settings
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<h4 class="text-center">Admin Settings page content here....</h4>
 @endsection
