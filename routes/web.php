580abac65ae3ba75b30e3fe51881f69d3a190693
osmanforhad <developer.osmanforhad@gmail.com>
2022-06-06 16:47:18 +0600

    basic rote setup as route group

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    Route::get('dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+    Route::get('profile', [AdminController::class, 'profile'])->name('admin.profile');
+    Route::get('settings', [AdminController::class, 'settings'])->name('admin.settings');
+});
+
+Route::group(['prefix' => 'user', 'middleware' => 'auth'], function() {
+    Route::get('dashboard', [UserController::class, 'index'])->name('admin.dashboard');
+    Route::get('profile', [UserController::class, 'profile'])->name('admin.profile');
+    Route::get('settings', [UserController::class, 'settings'])->name('admin.settings');
+});
