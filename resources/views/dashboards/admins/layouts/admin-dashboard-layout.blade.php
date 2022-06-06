89f514592fa530dbf2bf683ac2e56ff85315f1ba
osmanforhad <developer.osmanforhad@gmail.com>
2022-06-07 00:13:04 +0600

    setup active navigation in sidebar

--- resources/views/dashboards/admins/layouts/admin-dashboard-layout.blade.php
+++ resources/views/dashboards/admins/layouts/admin-dashboard-layout.blade.php
@@ -79,19 +79,19 @@
                     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                         data-accordion="false">
                         <li class="nav-item">
-                            <a href="{{ route('admin.dashboard') }}" class="nav-link">
+                            <a href="{{ route('admin.dashboard') }}" class="nav-link {{ (request()->is('admin/dashboard*')) ? 'active' : '' }}">
                                 <i class="nav-icon fas fa-home"></i>
                                 <p>Dashboard</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{ route('admin.profile') }}" class="nav-link">
+                            <a href="{{ route('admin.profile') }}" class="nav-link {{ (request()->is('admin/profile*')) ? 'active' : '' }}">
                                 <i class="nav-icon fas fa-user"></i>
                                 <p>Profiles</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{ route('admin.settings') }}" class="nav-link">
+                            <a href="{{ route('admin.settings') }}" class="nav-link {{ (request()->is('admin/settings*')) ? 'active' : '' }}">
                                 <i class="nav-icon fas fa-cog"></i>
                                 <p>Settings</p>
                             </a>
--- resources/views/dashboards/users/layouts/user-dashboard-layout.blade.php
+++ resources/views/dashboards/users/layouts/user-dashboard-layout.blade.php
@@ -79,19 +79,19 @@
                     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                         data-accordion="false">
                         <li class="nav-item">
-                            <a href="{{ route('user.dashboard') }}" class="nav-link">
+                            <a href="{{ route('user.dashboard') }}" class="nav-link {{ (request()->is('user/dashboard*')) ? 'active' : '' }}">
                                 <i class="nav-icon fas fa-home"></i>
                                 <p>Dashboard</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{ route('user.profile') }}" class="nav-link">
+                            <a href="{{ route('user.profile') }}" class="nav-link {{ (request()->is('user/profile*')) ? 'active' : '' }}">
                                 <i class="nav-icon fas fa-user"></i>
                                 <p>Profiles</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{ route('user.settings') }}" class="nav-link">
+                            <a href="{{ route('user.settings') }}" class="nav-link {{ (request()->is('user/settings*')) ? 'active' : '' }}">
                                 <i class="nav-icon fas fa-cog"></i>
                                 <p>Settings</p>
                             </a>
